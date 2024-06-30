251743cfb26c8c6b39130c54ce569189b9093cf3
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2024-06-30 23:53:20 +0100

    Installation program

---
+++ pages/Index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react-dom";
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main>
+        <div>
+          <h1>Homepage</h1>
+        </div>
+      </main>
+    </>
+  );
+}
